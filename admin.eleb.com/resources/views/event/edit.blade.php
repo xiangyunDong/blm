741bc01beab813a6c50f29164001ba07b9a4b135
ASUS <1033707637@qq.com>
2019-03-10 11:04:19 +0800

    完成

--- admin.eleb.com/resources/views/event/edit.blade.php
+++ admin.eleb.com/resources/views/event/edit.blade.php
@@ -3,9 +3,9 @@
     <h1>活动修改页面</h1>
     @include('layout._errors')
     @include('vendor.ueditor.assets')
-    <form class="form-group" method="post" action="{{route('activities.update',[$activity])}}">
+    <form class="form-group" method="post" action="{{route('events.update',[$event])}}">
         <label>活动名</label>
-        <input class="form-control" type="text" name="title" value="{{$activity->title}}">
+        <input class="form-control" type="text" name="title" value="{{$event->title}}">
         <label>内容</label>
         <script type="text/javascript">
             var ue = UE.getEditor('container');
@@ -13,11 +13,15 @@
                 ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
             });
         </script>
-        <script id="container" name="content" type="text/plain" >{!!$activity->content!!}</script>
-        <label>活动开始时间</label>
-        <input type="date" class="form-control" name="start_time" value="{{substr($activity->start_time,0,10)}}">
-        <label>活动结束时间</label>
-        <input type="date" class="form-control" name="end_time" value="{{substr($activity->end_time,0,10)}}">
+        <script id="container" name="content" type="text/plain" >{!!$event->content!!}</script>
+        <label>报名开始时间</label>
+        <input type="date" class="form-control" name="signup_start" value="{{date("Y-m-d",$event->signup_start)}}">
+        <label>报名结束时间</label>
+        <input type="date" class="form-control" name="signup_end" value="{{date("Y-m-d",$event->signup_end)}}">
+        <label>开奖日期</label>
+        <input type="date" class="form-control" name="prize_date" value="{{substr($event->prize_date,0,10)}}">
+        <label>报名人数限制</label>
+        <input type="text" class="form-control" name="signup_num" value="{{$event->signup_num}}">
         {{csrf_field()}}
         {{method_field('patch')}}
         <button class="btn bg-primary" type="submit">提交</button>
